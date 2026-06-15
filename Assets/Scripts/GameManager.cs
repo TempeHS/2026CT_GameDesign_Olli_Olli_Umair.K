f30f373c682ab9cdc5da27bba8e0c69b0edcb4bf
Umair K <umair.khan7@education.nsw.gov.au>
2026-06-15 11:39:47 +1000

    WIP: Collectable

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int Score; 
+
+    public text ScoreText;
+    string ScoreFormat = "{0, 5:00000}";
+
+    private void Awake()
+    {
+        Score = 0;
+        ScoreText.text = string.Format(ScoreFormat, Score);
+    }
+
+    void GoldenMushroomCollected()
+    {
+        Score += 25;
+    }
+}
