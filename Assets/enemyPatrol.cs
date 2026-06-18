d9288a3f7a616e8b70d5d7519a4c157989a4d900
Umair K <umair.khan7@education.nsw.gov.au>
2026-06-18 11:55:23 +1000

    WIP: EnemyMovement

---
+++ Assets/enemyPatrol.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class enemyPatrol : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isRunning", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+    }
+}
