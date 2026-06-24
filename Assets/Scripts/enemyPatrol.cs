7588b3fed616c4576ca4a787637a98292f5f05b0
Umair K <umair.khan7@education.nsw.gov.au>
2026-06-25 09:40:36 +1000

    WIP: EnemyPatrol

--- Assets/Scripts/enemyPatrol.cs
+++ Assets/Scripts/enemyPatrol.cs
@@ -23,11 +23,20 @@ public class enemyPatrol : MonoBehaviour
         Vector2 point = currentPoint.position - transform.position;
         if(currentPoint == pointB.transform)
         {
-            rb.velocity = new Vector2(speed, 0);
+            rb.linearVelocity = new Vector2(speed, 0);
         }
         else
         {
-            rb.velocity = new Vector2(-speed, 0);
+            rb.linearVelocity = new Vector2(-speed, 0);
+        }
+        
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
         }
     }
 }
