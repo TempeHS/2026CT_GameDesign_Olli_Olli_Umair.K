48510bfbbf241509de83a34914384f6ceed0da89
Umair K <umair.khan7@education.nsw.gov.au>
2026-09-10 11:53:22 +1000

    WIP: Audio

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
-{   
-    [Header("---------- Audio Source ----------")]
+{
+    [Header("---------- Audio Source ---------- ")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
-    [Header("---------- Audio Clip ----------")]
+    [Header("---------- Audio Clip ---------- ")]
     public AudioClip background;
     public AudioClip death;
     public AudioClip checkpoint;
-    public AudioClip wallTouch
+    public AudioClip wallTouch;
     public AudioClip portalIn;
     public AudioClip portalOut;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+    
+    
+ 
+ 
 }
